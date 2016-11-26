0805f34ab6b7f6bf2fdaad20de540df8981d6211
ifgeny87 <ifgeny87@gmail.com>
2016-11-27 02:54:33 +0500

    Реф тестов

--- tests/run_tests.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cls
-php -c c:/xampp/php/php.ini phpunit-5.6.5.phar --bootstrap ./bootstrap.php testlist/MilestoneTest.php > test.log
-echo ------------------------------------------ >> test.log
-php -c c:/xampp/php/php.ini phpunit-5.6.5.phar --bootstrap ./bootstrap.php testlist/TaskTest.php >> test.log
-more test.log
